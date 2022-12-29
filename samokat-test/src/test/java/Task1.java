2de0b07a8650e07a3e538cef9d3cd49e335fc40b
Ekaterina Lepekhina <katrin.le99@yandex.ru>
2022-12-29 23:27:20 +0300

    added tests

---
+++ samokat-test/src/test/java/Task1.java
@@ -0,0 +1,69 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import ru.praktikum_services.qa_scooter.HomePage;
+
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class Task1 {
+    private final static String WEB_LINK = "https://qa-scooter.praktikum-services.ru/";
+    private String question;
+    private String expectedAnswer;
+    private WebDriver driver;
+
+    public Task1(String question, String expectedAnswer) {
+        this.question = question;
+        this.expectedAnswer = expectedAnswer;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTestData() {
+        return new Object[][]{
+                {"Хочу сразу несколько самокатов! Так можно?",
+                        "Пока что у нас так: один заказ — один самокат." +
+                                " Если хотите покататься с друзьями," +
+                                " можете просто сделать несколько заказов — один за другим."},
+                {"Можно ли заказать самокат прямо на сегодня?",
+                        "Только начиная с завтрашнего дня. Но скоро станем расторопнее."}
+        };
+    }
+
+    @Before
+    public void init() {
+        driver = new ChromeDriver();
+        driver.get(WEB_LINK);
+    }
+
+    @Test
+    public void hasAccordingAnswerForQuestionTrue() throws Exception {
+        HomePage homePage = new HomePage(driver);
+        homePage.confirmCookies();
+        homePage.scrollToQuestions();
+        List<WebElement> questions = homePage.getListQuestions();
+        boolean result = false;
+        for (int i = 0; i < questions.size(); i++) {
+            if (questions.get(i).getText().equals(question)) {
+                questions.get(i).click();
+                try {
+                    result = homePage.getAccordingAnswerBy(i).getText().equals(expectedAnswer);
+                } catch (Exception e) {
+                    throw new Exception("ERROR! Web element is NOT FOUND");
+                }
+                break;
+            }
+        }
+        Assert.assertTrue(result);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
---
+++ samokat-test/src/test/java/Task2.java
@@ -0,0 +1,86 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import ru.praktikum_services.qa_scooter.HomePage;
+import ru.praktikum_services.qa_scooter.order.ConfirmOrderPage;
+import ru.praktikum_services.qa_scooter.order.OrderPage;
+import ru.praktikum_services.qa_scooter.order.OrderRentPage;
+import ru.praktikum_services.qa_scooter.order.SuccessfulOrder;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class Task2 {
+    private final String name;
+    private final String surName;
+    private final String address;
+    private final String metroStation;
+    private final String phoneNumber;
+    private final String date;
+    private final String daysForRent;
+    private final String color;
+    private WebDriver driver;
+    private final static String WEB_LINK = "https://qa-scooter.praktikum-services.ru/";
+
+    public Task2(String name,
+                 String surName,
+                 String address,
+                 String metroStation,
+                 String phoneNumber,
+                 String date,
+                 String daysForRent,
+                 String color) {
+        this.name = name;
+        this.surName = surName;
+        this.address = address;
+        this.metroStation = metroStation;
+        this.phoneNumber = phoneNumber;
+        this.date = date;
+        this.daysForRent = daysForRent;
+        this.color = color;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTestData() {
+        return new Object[][]{
+                {"Катя", "Лепехина", "г.Москва,ул.Басманная", "Бульвар Рокоссовского", "89999999999", "02.12.2022", "сутки", "grey"},
+                {"Иван", "Иванов", "г.Москва,ул.Проспект Вернадского", "Черкизовская", "89998889999", "02.09.2023", "трое суток", "black"}
+        };
+    }
+
+    @Before
+    public void init() {
+        driver = new ChromeDriver();//
+        driver.get(WEB_LINK);
+    }
+
+    @Test
+    public void checkOrderIsCreatedSuccessfullyTrue() throws Exception {
+        HomePage homePage = new HomePage(driver);
+        homePage.confirmCookies();
+        homePage.clickOrderButton();
+        OrderPage orderPage = new OrderPage(driver);
+        orderPage.createOrder(name, surName, address, metroStation, phoneNumber);
+        orderPage.clickContinueFillOrder();
+
+        OrderRentPage orderRentPage = new OrderRentPage(driver);
+        orderRentPage.fillRentInfo(date, daysForRent, color);
+        orderRentPage.saveOrder();
+
+        ConfirmOrderPage confirmOrderPage = new ConfirmOrderPage(driver);
+        confirmOrderPage.confirmOrder();
+
+        SuccessfulOrder successfulOrder = new SuccessfulOrder(driver);
+        boolean isOrderDone = successfulOrder.hasSuccessfulMsg();
+        assertTrue("ERROR! Order is NOT created successfully", isOrderDone);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
